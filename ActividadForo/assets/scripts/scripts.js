e38aa416e81acd1d798afe7183eb1b3b5d31719b
gontugithub <gs.alvarez8@gmail.com>
2024-03-10 21:56:10 +0100

    feat(ActividadForo): new user modal and basics functions close open created

--- ActividadForo/assets/scripts/scripts.js
+++ ActividadForo/assets/scripts/scripts.js
@@ -8,3 +8,11 @@ function fAbrirModalLogin(){
     document.querySelector("#modal_login").style.display = "flex";
     document.querySelector("#body_index").style.overflow = "hidden";
 }
+
+function fCerrarModalRegistro(){
+    document.querySelector("#modal_registro").style.display = "none";
+}
+
+function fAbrirModalRegistro(){
+    document.querySelector("#modal_registro").style.display = "flex";
+}
